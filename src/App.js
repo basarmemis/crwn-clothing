8fbd7c0b335e4e72d9d99b62c839201a2052e26d
basarmemis <basar.memis@gmail.com>
2020-06-09 00:46:45 +0300

    Added the Header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header-component';
+
 
 const HatsPage = () => (
   <div>
@@ -15,10 +17,12 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
+      
     </div>
   );
 }
